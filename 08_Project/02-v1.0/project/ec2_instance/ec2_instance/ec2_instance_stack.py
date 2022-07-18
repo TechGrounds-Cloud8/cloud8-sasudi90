2c4e1d32c36cc09daf7999bde254f23ce5fbdce7
sasudi90 <s.a.sudiarto@outlook.com>
2022-07-18 13:58:20 +0200

    added nacl for admin

--- 08_Project/02-v1.0/project/ec2_instance/ec2_instance/ec2_instance_stack.py
+++ 08_Project/02-v1.0/project/ec2_instance/ec2_instance/ec2_instance_stack.py
@@ -1,3 +1,4 @@
+from re import sub
 import aws_cdk
 from constructs import Construct
 from aws_cdk import (
@@ -6,8 +7,11 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_s3 as s3,
     aws_s3_deployment as s3deploy,
-    aws_s3_assets as Asset
+    aws_s3_assets as Asset,
+    aws_iam as iam,
 )
+
+my_ip="24.132.91.9/32"
 
 """with open("./postdeploymentscripts/user_data_web.sh", encoding="utf-8") as f:
     user_data = f.read()"""
@@ -56,7 +60,7 @@ class Ec2InstanceStack(Stack):
             allow_all_outbound=True,
         )
 
-        # APP_PROD_SG: add a new ingress rules to allow port 80 and 443
+        #APP_PROD_SG: add a new ingress rules to allow port 80 and 443
         app_prod_SG.add_ingress_rule(
             peer=ec2.Peer.any_ipv4(),
             description="Allow HTTP", 
@@ -87,10 +91,18 @@ class Ec2InstanceStack(Stack):
             machine_image=ec2.MachineImage().lookup(name="amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-gp2"),
             security_group=app_prod_SG,
             key_name="project_key_pair",
-            #user_data=ec2.UserData.custom(user_data),
-            vpc=vpc_web
+            # role=iam.Role(
+            #     self, 
+            #     "WebServerRole",
+            #     assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            #     description="Webserver role",
+            #     managed_policies=[iam.ManagedPolicy.from_managed_policy_name("AmazonS3ReadOnlyAccess")],
+            # ),
+            vpc=vpc_web,
         )
         
+        # instance_web.add_to_role_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess"))
+
         #################################
         ######## NACL Webserver #########
         #################################
@@ -158,6 +170,15 @@ class Ec2InstanceStack(Stack):
             rule_action=ec2.Action.ALLOW
         )
 
+        web_nacl.add_entry(
+            id="Inbound: SSH from my IP",
+            cidr=ec2.AclCidr.any_ipv4(),
+            rule_number=150,
+            traffic=ec2.AclTraffic.tcp_port(22),
+            direction=ec2.TrafficDirection.INGRESS,
+            rule_action=ec2.Action.ALLOW
+        )
+        
         #################################
         ######### SG MNMGTserver #########
         #################################
@@ -170,15 +191,16 @@ class Ec2InstanceStack(Stack):
         )
 
         admin_SG.add_ingress_rule(
-            peer=ec2.Peer.any_ipv4(),
+            peer=ec2.Peer.ipv4(my_ip),
             description="Allow SSH", 
             connection=ec2.Port.tcp(22)
         )
+
         #################################
         ####### Admin Instance #######
         #################################
 
-        instance_mngmt= ec2.Instance(
+        instance_admin= ec2.Instance(
             self,
             "admin instance",
             instance_name="adminserver",
@@ -190,44 +212,53 @@ class Ec2InstanceStack(Stack):
         )
 
         #################################
-        ######## NACL Webserver #########
+        ######## NACL Adminserver #########
         #################################
 
-        # network_acl_admin = ec2.NetworkAcl(
-        #     self,
-        #     "Admin_NACL",
-        #     vpc=vpc_web,
-        #     subnet_selection=ec2.SubnetSelection(
-        #         subnet_type=ec2.SubnetType.PUBLIC
-        #     )
-        # )      
+        network_acl_admin = ec2.NetworkAcl(
+            self,
+            "Admin_NACL",
+            vpc=vpc_admin,
+            subnet_selection=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PUBLIC
+            )
+        )      
 
-        # network_acl_admin.add_entry(
-        #     id="Inbound: SSH from anywhere",
-        #     cidr=ec2.AclCidr.any_ipv4(),
-        #     rule_number=100,
-        #     traffic=ec2.AclTraffic.tcp_port(22),
-        #     direction=ec2.TrafficDirection.INGRESS,
-        #     rule_action=ec2.Action.ALLOW
-        # )
+        network_acl_admin.add_entry(
+            id="Inbound: SSH from my IP",
+            cidr=ec2.AclCidr.ipv4(my_ip),
+            rule_number=200,
+            traffic=ec2.AclTraffic.tcp_port(22),
+            direction=ec2.TrafficDirection.INGRESS,
+            rule_action=ec2.Action.ALLOW
+        )
 
-        # network_acl_admin.add_entry(
-        #     "Inbound: Ephemeral ports from anywhere",
-        #     cidr=ec2.AclCidr.any_ipv4(),
-        #     rule_number=140,
-        #     traffic=ec2.AclTraffic.tcp_port_range(1024, 65535),
-        #     direction=ec2.TrafficDirection.INGRESS,
-        #     rule_action=ec2.Action.ALLOW
-        # )
+        network_acl_admin.add_entry(
+            id="Outbound: SSH to anywhere",
+            cidr=ec2.AclCidr.any_ipv4(),
+            rule_number=200,
+            traffic=ec2.AclTraffic.tcp_port(22),
+            direction=ec2.TrafficDirection.EGRESS,
+            rule_action=ec2.Action.ALLOW
+        )
 
-        # network_acl_admin.add_entry(
-        #     "Outbound: Ephemeral ports to anywhere",
-        #     cidr=ec2.AclCidr.any_ipv4(),
-        #     rule_number=140,
-        #     traffic=ec2.AclTraffic.tcp_port_range(1024, 65535),
-        #     direction=ec2.TrafficDirection.EGRESS,
-        #     rule_action=ec2.Action.ALLOW
-        # )
+        network_acl_admin.add_entry(
+            "Inbound: Ephemeral ports from anywhere",
+            cidr=ec2.AclCidr.any_ipv4(),
+            rule_number=210,
+            traffic=ec2.AclTraffic.tcp_port_range(1024, 65535),
+            direction=ec2.TrafficDirection.INGRESS,
+            rule_action=ec2.Action.ALLOW
+        )
+
+        network_acl_admin.add_entry(
+            "Outbound: Ephemeral ports to anywhere",
+            cidr=ec2.AclCidr.any_ipv4(),
+            rule_number=210,
+            traffic=ec2.AclTraffic.tcp_port_range(1024, 65535),
+            direction=ec2.TrafficDirection.EGRESS,
+            rule_action=ec2.Action.ALLOW
+        )
 
         #################################
         ########## VPC Peering ##########
@@ -239,24 +270,24 @@ class Ec2InstanceStack(Stack):
             peer_vpc_id=vpc_admin.vpc_id,
             vpc_id=vpc_web.vpc_id,
         )
-        
-        # for subnet in vpc_web.public_subnets:
-        #     ec2.CfnRoute(
-        #         self,
-        #         id=f"{subnet.subnet_id}", 
-        #         route_table_id=subnet.route_table.route_table_id,
-        #         destination_cidr_block="10.20.20.0/24",
-        #         vpc_peering_connection_id=vPCPeering_connection.ref,
-        #         )
 
-        # for subnet in vpc_admin.public_subnets:
-        #     ec2.CfnRoute(
-        #         self,
-        #         id=f"{subnet.subnet_id}",
-        #         route_table_id=subnet.route_table.route_table_id,
-        #         destination_cidr_block="10.10.10.0/24",
-        #         vpc_peering_connection_id=vPCPeering_connection.ref,
-        #         )
+        for subnet in vpc_web.public_subnets:
+            ec2.CfnRoute(
+                self,
+                id=f"${subnet.node.id}-PeerRoute",
+                route_table_id=subnet.route_table.route_table_id,
+                destination_cidr_block="10.20.20.0/24",
+                vpc_peering_connection_id=vPCPeering_connection.ref,
+            )
+
+        for subnet in vpc_admin.public_subnets:
+            ec2.CfnRoute(
+                self,
+                id=f"${subnet.node.id}-PeerRoute", 
+                route_table_id=subnet.route_table.route_table_id,
+                destination_cidr_block="10.10.10.0/24",
+                vpc_peering_connection_id=vPCPeering_connection.ref,
+            )
 
         #################################
         ########### S3 Bucket ###########
@@ -269,7 +300,6 @@ class Ec2InstanceStack(Stack):
             removal_policy=aws_cdk.RemovalPolicy.DESTROY,
             encryption=s3.BucketEncryption.S3_MANAGED,
             auto_delete_objects=True,
-            public_read_access=False,
         )
 
         #put the scripts in dir postdeploymentscripts into s3 bucket
